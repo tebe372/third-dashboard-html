f0e46e65dacf9e167dd56abaafba8e2f89ab46e1
tebe372 <tebebeyesusgaredew@gmail.com>
2023-05-30 18:52:25 -0400

    Finished pie and spline chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 (function () {
-  initCharts1();
-  initCharts2();
+  mixPieChart();
+  TrendlineChart();
   initCharts3();
   initCharts4();
 })();
@@ -13,14 +13,19 @@ function findTick(x, ticks, range) {
   }
 }
 
-function initCharts1() {
-  Highcharts.chart('pie-chart1', {
+function mixPieChart() {
+  Highcharts.chart('mix-pie-chart', {
     credits: {
       enabled: false,
     },
 
     chart: {
       type: 'pie',
+      style: {
+        fontFamily: "Inter",
+        fontWeight: 500,
+      },
+      height: '350px',
     },
 
     title: {
@@ -32,7 +37,7 @@ function initCharts1() {
       align: 'center',
       verticalAlign: 'middle',
       y: 12,
-      x: -70,
+      x: -130,
       style: {
         fontWeight: 600,
         fontSize: '12px',
@@ -41,9 +46,28 @@ function initCharts1() {
     },
 
     legend: {
+      useHTML: true,
       align: 'right',
       verticalAlign: 'middle',
       layout: 'vertical',
+      itemStyle: {
+        lineHeight: '25px',
+        maxWidth: '300px',
+        minWidth: '200px',
+      },
+      symbolHeight: 15,
+      symbolWidth: 30,
+      labelFormatter: function () {
+        return '<div style="position: relative; top: -6px; font-size:14px; color: #929EA3;">' + this.name + '&nbsp;&nbsp;&nbsp;<span style="color: #1E1E1E;">' + this.y + '%</span></div>';
+      }
+    },
+
+    tooltip: {
+      crosshairs: true,
+      borderRadius: 8,
+      formatter: function () {
+        return '<span style="color:' + this.color + '"> ● </span>' + ' ' + Intl.NumberFormat().format(this.y) + '%';
+      },
     },
 
     plotOptions: {
@@ -60,14 +84,18 @@ function initCharts1() {
     series: [{
       borderWidth: 0,
       borderRadius: 0,
-      innerSize: 160,
-      size: 220,
+      innerSize: 200,
+      size: 260,
+      showInLegend: true,
       states: {
         hover: {
           brightness: 0,
           halo: {
             opacity: 1,
           }
+        },
+        inactive: {
+          enabled: false,
         }
       },
       data: [
@@ -106,8 +134,9 @@ function initCharts1() {
       borderWidth: 0,
       borderRadius: 0,
       startAngle: 10,
-      innerSize: 90,
-      size: 150,
+      innerSize: 120,
+      size: 180,
+      showInLegend: false,
       states: {
         hover: {
           brightness: 0,
@@ -115,6 +144,9 @@ function initCharts1() {
             opacity: 1,
             size: 5,
           }
+        },
+        inactive: {
+          enabled: false,
         }
       },
       data: [
@@ -153,142 +185,91 @@ function initCharts1() {
   });
 }
 
-function initCharts2() {
-  Highcharts.chart('pie-chart2', {
+function TrendlineChart() {
+  Highcharts.chart('trend-line-chart', {
     credits: {
       enabled: false,
     },
 
     chart: {
-      type: 'pie',
+      type: 'spline',
+      spacing: [30, 20, 20, 20],
+      height: '350px',
     },
 
     title: {
-      text: '',
+      text: ''
     },
 
-    subtitle: {
-      text: 'Conversions',
-      align: 'center',
-      verticalAlign: 'middle',
-      y: 12,
-      x: -70,
-      style: {
-        fontWeight: 600,
-        fontSize: '12px',
-        color: '#8AA9B6',
+    xAxis: {
+      type: "datetime",
+      labels: {
+        overflow: "justify",
+        formatter: function () {
+          return Highcharts.dateFormat("%b %e", this.value);
+        },
+        style: {
+          color: "#898989",
+          fontSize: "12px",
+        },
+      },
+      tickWidth: 0,
+      lineWidth: 0,
+      tickInterval: 1000 * 3600 * 24 * 7,
+    },
+
+    yAxis: {
+      title: {
+        text: "",
+      },
+      lineWidth: 1,
+      lineColor: '#EAEAEA',
+      tickInterval: 1,
+      max: 5,
+      min: 0,
+      labels: {
+        formatter: function () {
+          return this.value + '%';
+        },
+        style: {
+          fontSize: "10px",
+        }
       }
-    },
-
-    legend: {
-      align: 'right',
-      verticalAlign: 'middle',
-      layout: 'vertical',
     },
 
     plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: false
-        },
-        showInLegend: true
-      }
+      spline: {
+        pointInterval: 1000 * 3600 * 24 * 7,
+        pointStart: Date.UTC(2023, 2, 13, 0, 0, 0),
+      },
+    },
+
+    tooltip: {
+      enabled: false,
+    },
+
+    legend: {
+      enabled: false,
     },
 
     series: [{
-      borderWidth: 0,
-      borderRadius: 0,
-      innerSize: 160,
-      size: 220,
-      states: {
-        hover: {
-          brightness: 0,
-          halo: {
-            opacity: 1,
-          }
-        }
+      marker: {
+        enabled: false,
       },
-      data: [
-        {
-          name: 'Segment',
-          y: 80.1,
-          color: '#5FA8D3',
-        },
-        {
-          name: 'Product Owned',
-          y: 1,
-          color: '#BEE9E8',
-        },
-        {
-          name: 'K-Cluster',
-          y: 15,
-          color: '#247BA0',
-        },
-        {
-          name: 'FTV',
-          y: 14,
-          color: '#1B98E0',
-        },
-        {
-          name: 'Default',
-          y: 2,
-          color: '#57CC99',
-        },
-        {
-          name: 'Behavioral',
-          y: 1,
-          color: '#B8BEDD',
-        },
-      ]
+      color: '#5FA8D3',
+      data: [0, 0.2, 0.3, 0.2, 0.02, 0.3, 0.02, '', '']
     }, {
-      borderWidth: 0,
-      borderRadius: 0,
-      startAngle: 10,
-      innerSize: 90,
-      size: 150,
-      states: {
-        hover: {
-          brightness: 0,
-          halo: {
-            opacity: 1,
-            size: 5,
-          }
-        }
+      marker: {
+        enabled: false,
       },
-      data: [
-        {
-          name: 'Segment',
-          y: 80.1,
-          color: '#5FA8D3',
-        },
-        {
-          name: 'Product Owned',
-          y: 1,
-          color: '#BEE9E8',
-        },
-        {
-          name: 'K-Cluster',
-          y: 15,
-          color: '#247BA0',
-        },
-        {
-          name: 'FTV',
-          y: 14,
-          color: '#1B98E0',
-        },
-        {
-          name: 'Default',
-          y: 2,
-          color: '#57CC99',
-        },
-        {
-          name: 'Behavioral',
-          y: 1,
-          color: '#B8BEDD',
-        },
-      ]
+      color: '#57CC99',
+      data: [0.6, 1, 1.2, 0.6, 1.1, 0.5, 0]
+    }, {
+      marker: {
+        enabled: false,
+      },
+      color: '#1B98E0',
+      data: [5, 0.1, 3.1, 0.2, 0.6, 0.1, 0.7]
     }]
   });
 }
