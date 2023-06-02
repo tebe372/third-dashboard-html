e5f0252ba907d46b8b8c3d204c92fea470b04611
tebe372 <tebebeyesusgaredew@gmail.com>
2023-06-01 23:34:28 -0400

    Finished comparison table

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,19 @@
   mixPieChart();
   TrendlineChart();
   ComparisonTable(
-    'comparison-table', 
-    ['ALGORITHM TYPE', 'SESSIONS', 'APPLICATION CONVERSION',	'CONVERSION RATE'],
+    'comparison-table',
+    ['ALGORITHM TYPE', 'SESSIONS', 'APPLICATION CONVERSION', 'CONVERSION RATE'],
     [
       ['Products Owned', 346912, 381, 0.11],
       ['Behavioral', 19054, 153, 0.8],
       ['Default', 60450, 100, 0.17],
-      ['FTV', 1797,	9, 0.5],
+      ['FTV', 1797, 9, 0.5],
       ['K-Cluster', 565, 9, 1.59],
       ['Segment', 4530, 9, 0.2]
     ],
     ['Grand Total', 433308, 661, 0.15]
   );
-  initCharts4();
+  MatrixBubbleChart();
 })();
 
 function findTick(x, ticks, range) {
@@ -23,6 +23,21 @@ function findTick(x, ticks, range) {
       return ticks[tickValue];
     }
   }
+}
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+  decimals = typeof decimals !== 'undefined' ? decimals : 0;
+  dec_point = typeof dec_point !== 'undefined' ? dec_point : '.';
+  thousands_sep = typeof thousands_sep !== 'undefined' ? thousands_sep : ',';
+  if (isNaN(number)) {
+    number = 0;
+  }
+  var parts = Number(number).toFixed(decimals).toString().split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
+  if (parts.join(dec_point) == '-0')
+    return 0;
+  else
+    return parts.join(dec_point);
 }
 
 function mixPieChart() {
@@ -207,6 +222,10 @@ function TrendlineChart() {
       type: 'spline',
       spacing: [30, 20, 20, 20],
       height: '350px',
+      style: {
+        fontFamily: "Inter",
+        fontWeight: 500,
+      },
     },
 
     title: {
@@ -293,26 +312,57 @@ function ComparisonTable(tableId, header, data, footer) {
       <table class="table">
         <thead>
           <tr>
-            <th scope="col">` + header[0] + `</th>
+            <th scope="col" class="align-top">` + header[0] + `</th>
             <th scope="col" colspan="2" class="align-top">` + header[1] + `</th>
-            <th scope="col" colspan="2">` + header[2] + `</th>
-            <th scope="col">` + header[3] + `</th>
+            <th scope="col" colspan="2" class="align-top">` + header[2] + `</th>
+            <th scope="col" class="align-top">` + header[3] + `</th>
           </tr>
         </thead>
         <tbody>`;
-  
+
+  let maxSession = data[0][1];
+  let minSession = data[0][1];
+  let maxConversion = data[0][2];
+  let minConversion = data[0][2];
+  let maxRate = data[0][3];
+  let minRate = data[0][3];
+
+  data.forEach(item => {
+    if (item[1] > maxSession)
+      maxSession = item[1];
+    if (item[1] < minSession)
+      minSession = item[1];
+
+    if (item[2] > maxConversion)
+      maxConversion = item[2];
+    if (item[2] < minConversion)
+      minConversion = item[2];
+
+    if (item[3] > maxRate)
+      maxRate = item[3];
+    if (item[3] < minRate)
+      minRate = item[3];
+  });
+
+
   data.forEach(row => {
     html += `
           <tr>
             <th scope="row" class="first-td">` + row[0] + `</th>
-            <td class="text-end">` + number_format(row[1]) + `</td>
-            <td class="text-start"><span class="bar"></span></td>
-            <td class="text-end">` + number_format(row[2]) + `</td>
-            <td class="text-start"><span class="bar"></span></td>
-            <td class="text-center percentage">` + row[3] + `%</td>
+            <td class="text-end">` + number_format(row[1]) + `</td>`;
+
+    const barWidth = 2 + (48 / parseFloat(maxSession - minSession)) * parseFloat(row[1] - minSession);
+    html += `<td class="text-start"><span class="bar" style="padding: 0px ` + parseInt(barWidth) + `px;"></span></td>
+            <td class="text-end">` + number_format(row[2]) + `</td>`;
+
+    const conversionWidth = 2 + (48 / parseFloat(maxConversion - minConversion)) * (row[2] - minConversion);
+    html += `<td class="text-start"><span class="bar" style="padding: 0px ` + parseInt(conversionWidth) + `px;"></span></td>`;
+
+    const transparency = 0.2 + (0.5 / (maxRate - minRate)) * (row[3] - minRate);
+    html += `<td class="text-center percentage align-middle" style="background-color: rgba(36,123,160,` + transparency + `);">` + row[3] + `%</td>
           </tr>`;
   });
-        
+
   html += `
         </tbody>
         <tfoot>
@@ -322,7 +372,7 @@ function ComparisonTable(tableId, header, data, footer) {
             <td></td>
             <td class="text-end">` + number_format(footer[2]) + `</td>
             <td></td>
-            <td class="text-center percentage">` + footer[3] + `</td>
+            <td class="text-center percentage">` + footer[3] + `%</td>
           </tr>
         </tfoot>
       </table>
@@ -331,157 +381,132 @@ function ComparisonTable(tableId, header, data, footer) {
   table.innerHTML = html;
 }
 
-function initCharts4() {
-  Highcharts.chart('pie-chart4', {
+function MatrixBubbleChart() {
+  Highcharts.chart('matrix-bubble-chart', {
     credits: {
       enabled: false,
     },
 
     chart: {
-      type: 'pie',
-    },
-
-    title: {
-      text: '',
-    },
-
-    subtitle: {
-      text: 'Conversions',
-      align: 'center',
-      verticalAlign: 'middle',
-      y: 12,
-      x: -70,
+      type: 'bubble',
       style: {
-        fontWeight: 600,
-        fontSize: '12px',
-        color: '#8AA9B6',
-      }
+        fontFamily: "Inter",
+        fontWeight: 500,
+      },
     },
 
     legend: {
-      align: 'right',
-      verticalAlign: 'middle',
-      layout: 'vertical',
+      enabled: false
+    },
+
+    title: {
+      text: ''
+    },
+
+    xAxis: {
+      lineColor: '#EAEAEA',
+      title: {
+        text: ''
+      },
+      labels: {
+        formatter: function () {
+          if (this.value)
+            return parseInt(this.value/1000) + 'k';
+          else
+            return 0;
+        },
+        style: {
+          color: "#898989",
+          fontSize: "12px",
+        },
+      },
+      tickWidth: 0,
+      plotLines: [{
+        color: '#EAEAEA',
+        dashStyle: 'Dot',
+        width: 2,
+        value: 100000,
+        zIndex: 1,
+        label: {
+          useHTML: true,
+          rotation: 0,
+          style: {
+            fontSize: '10px',
+            fontWeight: 400,
+            color: '#898989',
+            padding: '6px 9px',
+            boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.25)',
+            borderRadius: '8px'
+          },
+          text: 'Avg Lines',
+        },
+        zIndex: 3
+      }],
+      tickInterval: 50000,
+    },
+
+    yAxis: {
+      startOnTick: false,
+      endOnTick: false,
+      gridLineWidth: 0,
+      lineWidth: 1,
+      lineColor: '#EAEAEA',
+      title: {
+        text: ''
+      },
+      labels: {
+        formatter: function () {
+          return this.value + '%';
+        },
+        style: {
+          fontSize: "12px",
+        },
+      },
+      maxPadding: 0.2,
+      plotLines: [{
+        color: '#EAEAEA',
+        dashStyle: 'Dot',
+        width: 2,
+        value: 0.6,
+        zIndex: 1,
+        label: {
+          useHTML: true,
+          align: 'right',
+          style: {
+            fontSize: '10px',
+            fontWeight: 400,
+            color: '#898989',
+            padding: '6px 9px',
+            boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.25)',
+            borderRadius: '8px'
+          },
+          text: 'Avg Lines',
+          x: -10,
+          y: -14,
+        },
+        zIndex: 3
+      }],
     },
 
     plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: false
-        },
-        showInLegend: true
+      bubble: {
+        maxSize: '18%',
+        minSize: 5,
+        marker: {
+          fillOpacity: 1,
+        }
       }
     },
 
     series: [{
-      borderWidth: 0,
-      borderRadius: 0,
-      innerSize: 160,
-      size: 220,
-      states: {
-        hover: {
-          brightness: 0,
-          halo: {
-            opacity: 1,
-          }
-        }
-      },
       data: [
-        {
-          name: 'Segment',
-          y: 80.1,
-          color: '#5FA8D3',
-        },
-        {
-          name: 'Product Owned',
-          y: 1,
-          color: '#BEE9E8',
-        },
-        {
-          name: 'K-Cluster',
-          y: 15,
-          color: '#247BA0',
-        },
-        {
-          name: 'FTV',
-          y: 14,
-          color: '#1B98E0',
-        },
-        {
-          name: 'Default',
-          y: 2,
-          color: '#57CC99',
-        },
-        {
-          name: 'Behavioral',
-          y: 1,
-          color: '#B8BEDD',
-        },
-      ]
-    }, {
-      borderWidth: 0,
-      borderRadius: 0,
-      startAngle: 10,
-      innerSize: 90,
-      size: 150,
-      states: {
-        hover: {
-          brightness: 0,
-          halo: {
-            opacity: 1,
-            size: 5,
-          }
-        }
-      },
-      data: [
-        {
-          name: 'Segment',
-          y: 80.1,
-          color: '#5FA8D3',
-        },
-        {
-          name: 'Product Owned',
-          y: 1,
-          color: '#BEE9E8',
-        },
-        {
-          name: 'K-Cluster',
-          y: 15,
-          color: '#247BA0',
-        },
-        {
-          name: 'FTV',
-          y: 14,
-          color: '#1B98E0',
-        },
-        {
-          name: 'Default',
-          y: 2,
-          color: '#57CC99',
-        },
-        {
-          name: 'Behavioral',
-          y: 1,
-          color: '#B8BEDD',
-        },
-      ]
+        { x: 346912, y: 0.11, z: 5, name: 'Product Owned', color: '#BEE9E8'},
+        { x: 19054, y: 0.8, z: 5, name: 'Behavioral', color: '#5FA8D3'},
+        { x: 60450, y: 0.17, z: 5, name: 'Default', color: '#57CC99'},
+        { x: 1797, y: 0.5, z: 5, name: 'FTV', color: '#1B98E0'},
+        { x: 565, y: 1.59, z: 5, name: 'K-Cluster', color: '#1B98E0'},
+        { x: 4530, y: 0.2, z: 5, name: 'Segment', color: '#B8BEDD'}
+      ],
     }]
   });
-}
-
-function number_format(number, decimals, dec_point, thousands_sep) {
-  decimals = typeof decimals !== 'undefined' ? decimals : 0;
-  dec_point = typeof dec_point !== 'undefined' ? dec_point : '.';
-  thousands_sep = typeof thousands_sep !== 'undefined' ? thousands_sep : ',';
-  if (isNaN(number)) {
-    number = 0;
-  }
-  var parts = Number(number).toFixed(decimals).toString().split('.');
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
-  if (parts.join(dec_point) == '-0')
-    return 0;
-  else
-    return parts.join(dec_point);
 }
